cfb7996c70e2d81656f69073b37bb53ce4f16b28
David Owen <owend91@gmail.com>
2022-12-07 08:45:18 -0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by David Owen on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
